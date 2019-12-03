fc2ff87b22d3b7575a625fe6f04cf2f405d14521
alex_molodoy <sivtsov_a@inbox.ru>
2019-12-03 22:20:04 +0300

    module1-task3v3

--- src/main.js
+++ src/main.js
@@ -73,50 +73,63 @@ const createSortTemplate = () => {
 const createContentTemplate = () => {
   return (
     `<ul class="trip-days">
-      <li class="trip-days__item  day">
-        <div class="day__info">
-          <span class="day__counter">1</span>
-          <time class="day__date" datetime="2019-03-18">MAR 18</time>
-        </div>
-
-        <ul class="trip-events__list">
-          <li class="trip-events__item">
-            <div class="event">
-              <div class="event__type">
-                <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
-              </div>
-              <h3 class="event__title">Taxi to airport</h3>
-
-              <div class="event__schedule">
-                <p class="event__time">
-                  <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
-                  &mdash;
-                  <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
-                </p>
-                <p class="event__duration">1H 30M</p>
-              </div>
-
-              <p class="event__price">
-                &euro;&nbsp;<span class="event__price-value">20</span>
-              </p>
-
-              <h4 class="visually-hidden">Offers:</h4>
-              <ul class="event__selected-offers">
-                <li class="event__offer">
-                  <span class="event__offer-title">Order Uber</span>
-                  &plus;
-                  &euro;&nbsp;<span class="event__offer-price">20</span>
-                </li>
-              </ul>
-
-              <button class="event__rollup-btn" type="button">
-                <span class="visually-hidden">Open event</span>
-              </button>
-            </div>
-          </li>
-        </ul>`
+      </ul>`
   );
 };
+
+const createDayListTemplate = () => {
+  return (
+    `<li class="trip-days__item  day">
+    <div class="day__info">
+      <span class="day__counter">1</span>
+      <time class="day__date" datetime="2019-03-18">MAR 18</time>
+    </div>
+
+    <ul class="trip-events__list">
+      </ul>
+      </li>`
+  );
+};
+
+const createCardTemplate = () => {
+  return (
+    `<li class="trip-events__item">
+    <div class="event">
+      <div class="event__type">
+        <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+      </div>
+      <h3 class="event__title">Taxi to airport</h3>
+
+      <div class="event__schedule">
+        <p class="event__time">
+          <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
+          &mdash;
+          <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+        </p>
+        <p class="event__duration">1H 30M</p>
+      </div>
+
+      <p class="event__price">
+        &euro;&nbsp;<span class="event__price-value">20</span>
+      </p>
+
+      <h4 class="visually-hidden">Offers:</h4>
+      <ul class="event__selected-offers">
+        <li class="event__offer">
+          <span class="event__offer-title">Order Uber</span>
+          &plus;
+          &euro;&nbsp;<span class="event__offer-price">20</span>
+         </li>
+      </ul>
+
+      <button class="event__rollup-btn" type="button">
+        <span class="visually-hidden">Open event</span>
+      </button>
+    </div>
+  </li>`
+  );
+};
+
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -126,10 +139,15 @@ const siteTripInfoElement = document.querySelector(`.trip-info`);
 const siteTripControlsElement = document.querySelector(`.trip-controls`);
 const siteTripEventsElement = document.querySelector(`.trip-events`);
 
+
 render(siteTripInfoElement, createRouteTemplate(), `beforeend`);
 render(siteTripControlsElement, createMenuTemplate(), `beforeend`);
 render(siteTripControlsElement, createFilterTemplate(), `beforeend`);
 render(siteTripEventsElement, createSortTemplate(), `beforeend`);
+render(siteTripEventsElement, createContentTemplate(), `beforeend`);
+const siteTripDaysElement = siteTripEventsElement.querySelector(`.trip-days`);
+render(siteTripDaysElement, createDayListTemplate(), `beforeend`);
+const siteTripDaysCardsListElement = siteTripDaysElement.querySelector(`.trip-days__item`);
 for (let i = 0; i < 3; i++) {
-  render(siteTripEventsElement, createContentTemplate(), `beforeend`);
+  render(siteTripDaysCardsListElement, createCardTemplate(), `beforeend`);
 }
